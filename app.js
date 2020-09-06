7c6763333b21799b34592bd322bf184f3ad1b91c
Komisarov-D <62189887+komisarov-d@users.noreply.github.com>
2020-09-06 20:33:59 +0300

    Fin

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require('express')
-const app = express()
 const path = require('path')
+const {v4} = require('uuid')
+const app = express()
 
+let CONTACTS = [
+    {id: v4(), name: 'Dmitriy', value: '0663663661', marked: false}
+]
+app.use(express.json())
 
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json(CONTACTS)
+})
 
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id:v4(),marked:false }
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.status(201).json(CONTACTS[idx])
+})
 
-
+app.delete(`/api/contacts/:id`, (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message:'Contact was deleted'})
+})
 
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
 
-app.listen(3000, () => console.log('Server'))
+app.listen(3000, () => console.log('Server starts...'))
--- client/frontend.js
+++ client/frontend.js
@@ -1,9 +1,20 @@
-import Vue from "vue";
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+Vue.component('loader', {
+    template: `
+    <div style="display: flex;justify-content: center;align-items: center">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  `
+})
 
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: true,
             form: {
                 name: '',
                 value: ''
@@ -11,11 +22,52 @@ new Vue({
             contacts: []
         }
     },
-    methods: {
-        createContact() {
-            const {...contact} = this.form
-            this.contacts.push({...contact, id: Date.now()})
-            this.form.name = this.form.value = ''
+    computed: {
+        couldCreate() {
+            return this.form.value.trim() && this.form.name.trim()
         }
+    },
+    methods: {
+        async createContact() {
+            const {...contact} = this.form
+            const newContact = await request('/api/contacts', 'POST', contact)
+            this.contacts.push(newContact)
+            this.form.name = this.form.value = ''
+        },
+        async dropContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE')
+            this.contacts = this.contacts.filter(con => con.id !== id)
+
+        },
+        async markContact(id) {
+            const contact = this.contacts.find(c => c.id === id)
+           const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact, marked: true
+            })
+
+            contact.marked = updated.marked
+        }
+    },
+    async mounted() {
+        this.contacts = await request('/api/contacts')
+        this.loading = false
     }
 })
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+        const response = await fetch(url, {
+            method, headers, body
+        })
+        return await response.json()
+
+    } catch (e) {
+        console.warn('error', e.message)
+    }
+}
